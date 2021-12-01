7a9a6e962a318f4b27fdcdc49b2bc2d6a256b7f7
Anusha SP <anushaspga@gmail.com>
2021-12-01 21:44:59 +0530

    changes

--- app.js
+++ app.js
@@ -17,16 +17,12 @@ function errorHandler(error) {
 
  function clickHandler() {
   // outputDiv.innerText = "abcdefg" + txtInput.value //
-
   var inputText = txtInput.value;
-
   outputDiv.innerText = "Translation in progress..."
-
-
 fetch(getTranslationURL(inputText))
  .then(response => response.json())
   .then(json => {
-    var translatedText = json.contents.translated
+    var translatedText = json.contents.translated;
     outputDiv.innerText = translatedText
   })
    .catch(errorHandler)
